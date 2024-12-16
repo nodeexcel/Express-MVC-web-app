e9f58ee145d0beffac6f868fa8b97779ba4b4c3f
nodeexcel <atul.etech2011@gmail.com>
2024-12-16 11:30:06 +0100

    fine-tuned performance for large datasets

--- controllers/error.js
+++
@@ -1,9 +0,0 @@
-/*[ Display error page ]*/
-exports.get404Page = (req, res, next) => {
-  res.status(404).render("404", {
-    pageTitle: "Page Not Found",
-    path: "",
-    user: req.session.user,
-    url: req.headers.host + req.originalUrl,
-  });
-};
